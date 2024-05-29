0146b397ec8efd6f556fcf01570cdd80a372af5e
fdlnicolau <149716240+fdlnicolau@users.noreply.github.com>
2024-05-29 13:43:05 +0200

    Add files via upload

---
+++ src/utils.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicolau <lnicolau@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 04:50:41 by lnicolau          #+#    #+#             */
+/*   Updated: 2024/05/26 04:50:41 by lnicolau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+int first_index_valid(char *str)
+{
+    int index;
+    while(str[index] == ' ' || str[index] == '\t'
+         || str[index] == '\v' || str[index] == '\r')
+        index++;
+    return (index);
+}
+
+int count_comp(char **map, char c)
+{
+	int i;
+	int j;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j] != '\0')
+		{
+			if (map[i][j] == c)
+				count++;
+			j++;
+		}
+		i++;
+	}
+	return (count);
+}
